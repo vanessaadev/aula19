de4f68d780c66e1c395b30e646990bb7f8ac6629
vanessa <vanessaaraujo77077@gmail.com>
2023-02-24 12:07:31 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function selecionar(input) {
+    // document.getElementById('quantidade').innerHTML = "1";
+    // document.querySelector('#quantidade').innerHTML = "1";
+    // document.querySelector('[id="quantidade"]').innerHTML = "1";
+
+    if (input.checked) {
+        quantidade.innerHTML = "1";
+    } else {
+        quantidade.innerHTML = "0";
+    }
+}
